fbbb3fe8af8c69c0b667080c244ad5356b30a99c
Piotr Solnica <piotr.solnica+oss@gmail.com>
2016-07-25 09:44:24 +0200

    Bump to rom 2.0

--- spec/shared/db_setup.rb
+++ spec/shared/db_setup.rb
@@ -1,5 +1,5 @@
 shared_context 'db setup' do
   let(:gateway) { container.gateways[:default] }
-  let(:configuration) { ROM::Configuration.new(:rethinkdb, db_options.merge(db: 'test_db')).use(:macros) }
+  let(:configuration) { ROM::Configuration.new(:rethinkdb, db_options.merge(db: 'test_db')) }
   let(:container) { ROM.container(configuration) }
 end
